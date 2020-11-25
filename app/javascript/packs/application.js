c5a3c535ba3121dc877f4f21db2a3d894def3d6f
ikaruga <ikaruga777@gmail.com>
2020-11-26 01:05:48 +0900

    せっかくなのでbootstrap-honokaをいれる

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,5 @@ require("@rails/activestorage").start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require popper
+//= require bootstrap-sprockets
